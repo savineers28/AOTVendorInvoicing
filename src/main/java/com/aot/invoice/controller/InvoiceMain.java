3e11cfbdaca0d932fae36ccb1ab4507d2f130942
savineers28 <cs4sys@gmail.com>
2020-04-28 20:26:13 +0530

    Initial Commit

--- src/main/java/com/aot/invoice/controller/InvoiceMain.java
+++ src/main/java/com/aot/invoice/controller/InvoiceMain.java
@@ -12,8 +12,10 @@ import static com.aot.invoice.topic.PaymentInfoReceiver.getPaymentInfoFromTopic;
 
 public class InvoiceMain {
     public static void main(String[] args)  {
+        System.out.println("Inside Invoice main");
         final Path dir = Paths.get(InvoiceConstants.invoiceInboundPath);
         ExecutorService executor = Executors.newFixedThreadPool(2);
+        System.out.println("Calling File Watcher");
         executor.submit(() -> {
             try {
                 new FileWatcher(dir);
@@ -21,6 +23,7 @@ public class InvoiceMain {
                 e.printStackTrace();
             }
         });
+        System.out.println("Looking for Payment Info");
         executor.submit(()-> {
             for(;;) {
                 try {
